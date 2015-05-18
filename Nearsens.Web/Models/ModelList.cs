b340fd40ddf134b94ae6859ccb6f928ebad01237
Simone <simone.morea@hotmail.it>
2015-05-18 18:10:20 +0200

    Aggiunta lista luoghi, dettaglio, inserimento, cancellazione

---
+++ Nearsens.Web/Models/ModelList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nearsens.Models;
+
+namespace Nearsens.Web.Models
+{
+    public class ModelList
+    {
+        public IEnumerable<GetPlacesByUserIdQuery> places;
+        public GetPlaceQuery place;
+        public IEnumerable<Offer> offersByPlace;
+        public Offer offerDetail;
+        public IEnumerable<string> subcategories;
+    }
+}
